52eee36af2144f074bda97f10b7435069b6b610f
João Vitor Dalcastanher Dell'Agnolo <80483816+JoaoDalcastanher@users.noreply.github.com>
2026-05-12 10:53:51 -0300

    Restore la/translation.py with correct Latin title for Pasc5-3

--- backend/api/constants/la/translation.py
+++ backend/api/constants/la/translation.py
@@ -1,1 +1,25 @@
-PLACEHOLDER_LA
+import re
+
+from api.constants import common as constants
+from api.constants.common import TRANSFORMATIONS_COMMON
+
+PDF_LABELS = {
+    "rank": "Ritus",
+    "colors": "Color"
+}
+
+PDF_RANK_LABELS = {
+    1: "Classis I",
+    2: "Classis II",
+    3: "Classis III",
+    4: "Classis IV",
+}
+
+PDF_COLOR_LABELS = {
+    "g": "Virides",
+    "r": "Rubri",
+    "w": "Albi",
+    "v": "Violacei",
+    "p": "Rosacei",
+    "b": "Nigri",
+}
